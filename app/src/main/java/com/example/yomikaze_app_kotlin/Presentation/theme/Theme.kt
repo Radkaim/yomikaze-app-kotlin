680aa083291852b8a4f7c9fa37c5ced6a98d3ad6
DaoNhutThi <114686399+DaoNhutThi@users.noreply.github.com>
2024-05-11 17:17:14 +0700

    update theme

--- app/src/main/java/com/example/yomikaze_app_kotlin/Presentation/theme/Theme.kt
+++ app/src/main/java/com/example/yomikaze_app_kotlin/Presentation/theme/Theme.kt
@@ -16,15 +16,21 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+//    primary = Purple80,
+//    secondary = PurpleGrey80,
+//    tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    background = Green100,
+    onBackground = Green900,
+    primary = Green100,
+    onPrimary = Red900,
+
+    secondary = Red100,
+    onSecondary = Red900,
+    error = Red700,
+    onError = Red100,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
