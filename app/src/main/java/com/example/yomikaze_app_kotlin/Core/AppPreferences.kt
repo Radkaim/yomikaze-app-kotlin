49d20acc8563210596819435e58a20582a1e8438
Radkaim <hungct04@gmail.com>
2024-05-19 14:16:59 +0700

    update AppPreferences

--- app/src/main/java/com/example/yomikaze_app_kotlin/Core/AppPreferences.kt
+++ app/src/main/java/com/example/yomikaze_app_kotlin/Core/AppPreferences.kt
@@ -1,9 +1,11 @@
 package com.example.yomikaze_app_kotlin.Core
 
+import android.content.Context
+import android.content.SharedPreferences
 import com.example.yomikaze_app_kotlin.ui.AppTheme
 import io.paperdb.Paper
 
-object AppPreferences {
+object AppThemeSate {
     const val APP_THEME = "AppTheme"
 
 
@@ -16,4 +18,27 @@ object AppPreferences {
         return Paper.book().read(APP_THEME, AppTheme.Default) ?: AppTheme.Default
     }
 
+}
+class AppPreferences(context : Context) {
+    private val preferences: SharedPreferences = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+
+    // save user token
+    var userToken: String?
+        get() = preferences.getString("user_token", null)
+        set(value) = preferences.edit().putString("user_token", value).apply()
+
+    // delete user token
+    fun deleteUserToken() {
+        preferences.edit().remove("user_token").apply()
+    }
+
+    // save app theme
+    var appTheme: String?
+        get() = preferences.getString("app_theme", null)
+        set(value) = preferences.edit().putString("app_theme", value).apply()
+
+    // delete app theme
+    fun deleteAppTheme() {
+        preferences.edit().remove("app_theme").apply()
+    }
 }
