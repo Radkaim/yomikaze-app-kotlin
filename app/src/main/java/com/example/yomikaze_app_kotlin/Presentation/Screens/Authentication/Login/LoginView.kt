b6870efa809b0a48f8f3ceda2b94ece02db68c88
DaoNhutThi <114686399+DaoNhutThi@users.noreply.github.com>
2024-06-04 10:34:04 +0700

    add icon, splashScreen.kt

--- app/src/main/java/com/example/yomikaze_app_kotlin/Presentation/Screens/Authentication/Login/LoginView.kt
+++ app/src/main/java/com/example/yomikaze_app_kotlin/Presentation/Screens/Authentication/Login/LoginView.kt
@@ -1,24 +1,38 @@
 package com.example.yomikaze_app_kotlin.Presentation.Screens.Authentication.Login
 
+import android.media.Image
 import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.yomikaze_app_kotlin.R
 
 
 @Composable
@@ -26,45 +40,81 @@ fun LoginView(loginViewModel: LoginViewModel = hiltViewModel()) {
     val state by loginViewModel.state.collectAsState()
     LoginContent(state, loginViewModel)
 }
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginContent(state: LoginState, viewModel: LoginViewModel) {
 
-    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+    Box(
+        modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)
+        .background(color = MaterialTheme.colors.background),
+    ) {
         var email by remember { mutableStateOf("") }
         var password by remember { mutableStateOf("") }
         state.hung = "hung"
 
-        TextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Email") },
-            modifier = Modifier.fillMaxWidth().padding(bottom = 8.dp)
-        )
+        Column(
+//            Alignment = Alignment.Center,
 
-        TextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Password") },
-            modifier = Modifier.fillMaxWidth().padding(bottom = 8.dp),
-            visualTransformation = PasswordVisualTransformation()
-        )
-        Button(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
 
-            content = {
-                Text("Login")
-            },
+        ) {
+            Image(painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .height(200.dp)
+                    .width(100.dp),
+                contentScale = ContentScale.Fit
+            )
+
+            OutlinedTextField(value = email,
+                onValueChange = {email = it},
+                label = { Text(text = "Email")},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp),
+//                visualTransformation = PasswordVisualTransformation()
+            )
+
+            OutlinedTextField(value = password,
+                onValueChange = {password = it},
+                label = { Text(text = "Password")},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp),
+                visualTransformation = PasswordVisualTransformation()
+                )
+
+            Button(
+                content = {
+                    Text("Login")
+
+                },
+//                Color = androidx.compose.material3.MaterialTheme.colorScheme.onPrimary,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .width(50.dp)
+                    .height(40.dp),
+
+//                    .background(color = MaterialTheme.colors.primary),
 
 
-            onClick = { viewModel.onLogin(email, password)
+                onClick = { viewModel.onLogin(email, password)
 
-            },
-            modifier = Modifier.fillMaxWidth()
+                },
 //        ) {
 ////            if (state.isLoading) {
 ////                CircularProgressIndicator(modifier = Modifier.size(24.dp))
 ////            }
 //        })
-        )
+            )
+        }
+//
+
+
 
         state.error?.let {
             Text(
--- app/src/main/java/com/example/yomikaze_app_kotlin/Presentation/Screens/Splash/SplashScreen.kt
+++ app/src/main/java/com/example/yomikaze_app_kotlin/Presentation/Screens/Splash/SplashScreen.kt
@@ -5,13 +5,18 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.yomikaze_app_kotlin.R
 import kotlinx.coroutines.delay
@@ -42,10 +47,15 @@ fun SplashScreen(navController: NavController){
     // Image
     Box(
         contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()) {
-        Image(painter = painterResource(id = R.drawable.icon_home),
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(painter = painterResource(id = R.drawable.logo),
             contentDescription = "Logo",
-            modifier = Modifier.scale(scale.value))
+            modifier = Modifier
+                .height(200.dp)
+                .width(100.dp),
+            contentScale = ContentScale.Fit
+        )
     }
 
 }
