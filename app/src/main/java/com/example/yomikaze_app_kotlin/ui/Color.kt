8f1f7bc24272178a10160a191a8e3bb55ab2638e
Radkaim <hungct04@gmail.com>
2024-06-04 15:27:38 +0700

    update SplashScreen

--- app/src/main/java/com/example/yomikaze_app_kotlin/ui/Color.kt
+++ app/src/main/java/com/example/yomikaze_app_kotlin/ui/Color.kt
@@ -2,8 +2,13 @@ package com.example.yomikaze_app_kotlin.ui
 
 import androidx.compose.ui.graphics.Color
 
+//lightColorScheme
 //Background gray
-val Gray100 = Color(0xffD9D9D9).copy(alpha = 0.7f)
+val GrayA07 = Color(0xffD9D9D9).copy(alpha = 0.7f)
+val red = Color(0xffFF0000)
+val white = Color(0xffFFFFFF)
+val OrangeSplash = Color(0xffE38B6C)
+val GreenSplash = Color(0xff65F6D1)
 
 //Green
 val Green100 = Color(0xffF2FFFD)
@@ -17,18 +22,18 @@ val Green800 = Color(0xff004E24)
 val Green900 = Color(0xff00260F)
 
 //Red
-val Red100 = Color(0xffFFF7F2)
-val Red200 = Color(0xffFFDACA)
-val Red300 = Color(0xffFBB69F)
-val Red400 = Color(0xffF28C74)
-val Red500 = Color(0xffE05C48)
-val Red600 = Color(0xffB22F24)
-val Red700 = Color(0xff831513)
-val Red800 = Color(0xff550A0A)
-val Red900 = Color(0xff260505)
+val Orange100 = Color(0xffFFF7F2)
+val Orange200 = Color(0xffFFDACA)
+val Orange300 = Color(0xffFBB69F)
+val Orange400 = Color(0xffF28C74)
+val Orange500 = Color(0xffE05C48)
+val Orange600 = Color(0xffB22F24)
+val Orange700 = Color(0xff831513)
+val Orange800 = Color(0xff550A0A)
+val Orange900 = Color(0xff260505)
 
-//diff
-val red = Color(0xffFF0000)
-val gray = Color(0xffd9d9d9)
-val white = Color(0xffFFFFFF)
 
+
+
+
+// darkColorScheme
