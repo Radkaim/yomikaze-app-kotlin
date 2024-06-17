4c61b458a6f83c9380ce9ade3219aa99d9783752
Radkaim <hungct04@gmail.com>
2024-06-17 18:22:22 +0700

    update for rankin Comment, follow, rating ranking

--- app/src/main/java/com/example/yomikaze_app_kotlin/Presentation/Screens/Ranking/ViewFollow/FollowComicView.kt
+++ app/src/main/java/com/example/yomikaze_app_kotlin/Presentation/Screens/Ranking/ViewFollow/FollowComicView.kt
@@ -1,24 +1,179 @@
 package com.example.yomikaze_app_kotlin.Presentation.Screens.Ranking.ViewFollow
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.yomikaze_app_kotlin.Presentation.Components.ComicCard.RankingComicCard.NormalComicCard
+import com.example.yomikaze_app_kotlin.Domain.Model.Comic
+import com.example.yomikaze_app_kotlin.Presentation.Components.ComicCard.RankingComicCard.RankingComicCard
 
 @Composable
 fun FollowComicView() {
+
+
+    /**
+     * Test Normal comic for library
+     */
+//    Column(
+//        verticalArrangement = Arrangement.spacedBy(15.dp), // 8.dp space between each card
+//        modifier = Modifier
+//            .padding(
+//                top = 15.dp,
+//                start = 4.dp,
+//                end = 4.dp,
+//                bottom = 4.dp
+//            ) // Optional padding for the entire list
+//            .background(MaterialTheme.colorScheme.background)
+//            .wrapContentSize(Alignment.Center)
+//    ) {
+//
+//        // consume the NormalComicCard composable
+//        NormalComicCard(
+//            comicId = 1,
+//            image = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.6HUddKnrAhVipChl6084pwHaLH%26pid%3DApi&f=1&ipt=303f06472dd41f68d97f5684dc0d909190ecc880e7648ec47be6ca6009cbb2d1&ipo=images",
+//            comicName = "Hunter X Hunter",
+//            status = "On Going",
+//            authorName = "Yoshihiro Togashi",
+//            publishedDate = "1998-03-03",
+//            ratingScore = 9.5f,
+//            follows = 100,
+//            views = 100,
+//            comments = 100,
+//            isDeleted = true,
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .height(119.dp)
+//                .border(
+//                    width = 1.dp,
+//                    color = MaterialTheme.colorScheme.onSecondary.copy(alpha = 0.3f),
+//                    shape = MaterialTheme.shapes.small
+//                ),
+//
+//            )
+//        NormalComicCard(
+//            comicId = 2,
+//            image = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.6HUddKnrAhVipChl6084pwHaLH%26pid%3DApi&f=1&ipt=303f06472dd41f68d97f5684dc0d909190ecc880e7648ec47be6ca6009cbb2d1&ipo=images",
+//            comicName = "Hunter X Hunter67677667",
+//            status = "On Going",
+//            authorName = "Yoshihiro Togashi",
+//            publishedDate = "1998-03-03",
+//            ratingScore = 9.5f,
+//            follows = 100,
+//            views = 100,
+//            comments = 100,
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .height(126.dp)
+//                .border(
+//                    width = 1.dp,
+//                    color = MaterialTheme.colorScheme.onSecondary.copy(alpha = 0.3f),
+//                    shape = MaterialTheme.shapes.small
+//                ),
+//        )
+//    }
+
+    //create listOf comics
+    val comicsListCardModel = listOf(
+        Comic(
+            comicId = 1,
+            rankingNumber = 1,
+            image = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.6HUddKnrAhVipChl6084pwHaLH%26pid%3DApi&f=1&ipt=303f06472dd41f68d97f5684dc0d909190ecc880e7648ec47be6ca6009cbb2d1&ipo=images",
+            comicName = "Hunter X Hunter",
+            status = "On Going",
+            authorName = "Yoshihiro Togashi",
+            publishedDate = "1998-03-03",
+            ratingScore = 9.5f,
+            follows = 8000000000,
+            views = 100000000000,
+            comments = 80000000000
+        ),
+        Comic(
+            comicId = 2,
+            rankingNumber = 2,
+            image = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.6HUddKnrAhVipChl6084pwHaLH%26pid%3DApi&f=1&ipt=303f06472dd41f68d97f5684dc0d909190ecc880e7648ec47be6ca6009cbb2d1&ipo=images",
+            comicName = "Hunter X Hunter12323",
+            status = "On Going",
+            authorName = "Yoshihiro Togashi",
+            publishedDate = "1998-03-03",
+            ratingScore = 9.5f,
+            follows = 100,
+            views = 100,
+            comments = 10
+        ),
+        Comic(
+            comicId = 3,
+            rankingNumber = 3,
+            image = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.6HUddKnrAhVipChl6084pwHaLH%26pid%3DApi&f=1&ipt=303f06472dd41f68d97f5684dc0d909190ecc880e7648ec47be6ca6009cbb2d1&ipo=images",
+            comicName = "Hunter X Hunter12323",
+            status = "On Going",
+            authorName = "Yoshihiro Togashi",
+            publishedDate = "1998-03-03",
+            ratingScore = 9.5f,
+            follows = 100,
+            views = 100,
+            comments = 10
+        ),  Comic(
+            comicId = 4,
+            rankingNumber = 4,
+            image = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.6HUddKnrAhVipChl6084pwHaLH%26pid%3DApi&f=1&ipt=303f06472dd41f68d97f5684dc0d909190ecc880e7648ec47be6ca6009cbb2d1&ipo=images",
+            comicName = "Hunter X Hunter12323",
+            status = "On Going",
+            authorName = "Yoshihiro Togashi",
+            publishedDate = "1998-03-03",
+            ratingScore = 9.5f,
+            follows = 100,
+            views = 100,
+            comments = 10
+        ),  Comic(
+            comicId = 5,
+            rankingNumber = 5,
+            image = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.6HUddKnrAhVipChl6084pwHaLH%26pid%3DApi&f=1&ipt=303f06472dd41f68d97f5684dc0d909190ecc880e7648ec47be6ca6009cbb2d1&ipo=images",
+            comicName = "Hunter X Hunter12323",
+            status = "On Going",
+            authorName = "Yoshihiro Togashi",
+            publishedDate = "1998-03-03",
+            ratingScore = 9.5f,
+            follows = 100,
+            views = 100,
+            comments = 10
+        ),  Comic(
+            comicId = 6,
+            rankingNumber = 6,
+            image = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.6HUddKnrAhVipChl6084pwHaLH%26pid%3DApi&f=1&ipt=303f06472dd41f68d97f5684dc0d909190ecc880e7648ec47be6ca6009cbb2d1&ipo=images",
+            comicName = "Hunter X Hunter12323",
+            status = "On Going",
+            authorName = "Yoshihiro Togashi",
+            publishedDate = "1998-03-03",
+            ratingScore = 9.5f,
+            follows = 100,
+            views = 100,
+            comments = 10
+        ),  Comic(
+            comicId = 7,
+            rankingNumber = 7,
+            image = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.6HUddKnrAhVipChl6084pwHaLH%26pid%3DApi&f=1&ipt=303f06472dd41f68d97f5684dc0d909190ecc880e7648ec47be6ca6009cbb2d1&ipo=images",
+            comicName = "Hunter X Hunter12323",
+            status = "On Going",
+            authorName = "Yoshihiro Togashi",
+            publishedDate = "1998-03-03",
+            ratingScore = 9.5f,
+            follows = 100,
+            views = 100,
+            comments = 10
+        ),
+    )
+
     Column(
-        verticalArrangement = Arrangement.spacedBy(15.dp), // 8.dp space between each card
+        verticalArrangement = Arrangement.spacedBy(15.dp), // 15.dp space between each card
         modifier = Modifier
             .padding(
                 top = 15.dp,
@@ -29,49 +184,31 @@ fun FollowComicView() {
             .background(MaterialTheme.colorScheme.background)
             .wrapContentSize(Alignment.Center)
     ) {
-
-        // consume the NormalComicCard composable
-        NormalComicCard(
-            comicId = 1,
-            image = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.6HUddKnrAhVipChl6084pwHaLH%26pid%3DApi&f=1&ipt=303f06472dd41f68d97f5684dc0d909190ecc880e7648ec47be6ca6009cbb2d1&ipo=images",
-            comicName = "Hunter X Hunter",
-            status = "On Going",
-            authorName = "Yoshihiro Togashi",
-            publishedDate = "1998-03-03",
-            ratingScore = 9.5f,
-            follows = 100,
-            views = 100,
-            comments = 100,
-            isDeleted = true,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(119.dp)
-                .border(
-                    width = 1.dp,
-                    color = MaterialTheme.colorScheme.onSecondary.copy(alpha = 0.3f),
-                    shape = MaterialTheme.shapes.small
-                ),
-
-            )
-        NormalComicCard(
-            comicId = 2,
-            image = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.6HUddKnrAhVipChl6084pwHaLH%26pid%3DApi&f=1&ipt=303f06472dd41f68d97f5684dc0d909190ecc880e7648ec47be6ca6009cbb2d1&ipo=images",
-            comicName = "Hunter X Hunter67677667",
-            status = "On Going",
-            authorName = "Yoshihiro Togashi",
-            publishedDate = "1998-03-03",
-            ratingScore = 9.5f,
-            follows = 100,
-            views = 100,
-            comments = 100,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(126.dp)
-                .border(
-                    width = 1.dp,
-                    color = MaterialTheme.colorScheme.onSecondary.copy(alpha = 0.3f),
-                    shape = MaterialTheme.shapes.small
-                ),
-        )
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(8.dp) // 8.dp space between each item
+        ) {
+            items(comicsListCardModel) { comic ->
+                RankingComicCard(
+                    comicId = comic.comicId,
+                    rankingNumber = comic.rankingNumber,
+                    image = comic.image,
+                    comicName = comic.comicName,
+                    status = comic.status,
+                    authorName = comic.authorName,
+                    publishedDate = comic.publishedDate,
+                    ratingScore = comic.ratingScore,
+                    follows = comic.follows,
+                    views = comic.views,
+                    comments = comic.comments,
+                    backgroundColor = MaterialTheme.colorScheme.onSurface,
+                    modifier = Modifier.clickable {
+                        //navController.navigate("comicDetail/${comic.comicId}")
+                      //  hotComicViewModel.navigateToComicDetail(comic.comicId)
+                        // TODO change to viewModel.navigateToComicDetail(comic.comicId) if using viewModel
+                    }
+                )
+            }
+        }
     }
+
 }
