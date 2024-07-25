4b65f54f7b13d463eae03af99ba957c4a2b359ce
Radkaim <hungct04@gmail.com>
2024-07-26 00:11:32 +0700

    update sign in view add icon gg

--- app/src/main/java/com/example/yomikaze_app_kotlin/Presentation/Screens/Authentication/Login/LoginView.kt
+++ app/src/main/java/com/example/yomikaze_app_kotlin/Presentation/Screens/Authentication/Login/LoginView.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -405,6 +406,12 @@ fun LoginContent(state: LoginState, loginViewModel: LoginViewModel, navControlle
 
                     )
                 {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_google),
+                        contentDescription = "Logo",
+                        modifier = Modifier.size(20.dp).offset(x = -(10).dp, y = 0.dp),
+                    )
+
                     Text(
                         text = "Login with Google",
                         color = Color.White,
