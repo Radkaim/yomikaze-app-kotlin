f3bad226f2dc9e5b7804d592885d09111debd095
Radkaim <hungct04@gmail.com>
2024-08-02 14:09:30 +0700

    update change password

--- app/src/main/java/com/example/yomikaze_app_kotlin/Domain/Models/Error.kt
+++ app/src/main/java/com/example/yomikaze_app_kotlin/Domain/Models/Error.kt
@@ -6,12 +6,3 @@ data class ErrorResponse(
     val message: String,
     val errors: Map<String, List<String>>? = null
 )
-
-data class ErrorDetail(
-    val code: String,
-    val description: String
-)
-
-data class ErrorPasswordChange(
-    val errors: List<ErrorDetail>
-)
